5531f5a6e1ac5aca286ba560ff0b661df54ae405
kpsf <kprodehl@salesforce.com>
2017-04-21 14:07:54 -0700

    allow CORS

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'redcloth'
 
 get '/' do
-  headers['Access-Control-Allow-Origin'] = "*"
   File.read('index.html')
 end
 
@@ -10,4 +9,9 @@ get '/parse' do
   body = params[:textile] || ""
   body = body.gsub("??", "<notextile>??</notextile>")
   RedCloth.new(body, [:filter_html, :filter_styles, :filter_classes, :filter_ids, :no_span_caps]).to_html
+end
+
+options '*' do
+ response.headers["Access-Control-Allow-Origin"] = "*"
+ halt 200
 end
