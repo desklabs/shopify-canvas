26dd0382b627ab717e33735e19250518887ed1f4
kpsf <kprodehl@salesforce.com>
2017-07-20 14:12:22 -0700

    session authorization and code cleanup

--- app.rb
+++ app.rb
@@ -5,41 +5,43 @@ require 'openssl'
 require 'Base64'
 require 'json'
 
-api_key  = ENV['SHOPIFY_API_KEY']
-api_pass = ENV["SHOPIFY_API_SECRET"]
-host     = ENV["SHOPIFY_HOST"]
-shop_url = "https://#{api_key}:#{api_pass}@#{host}.myshopify.com/admin"
-puts shop_url
+shop_url = "https://#{ENV['SHOPIFY_API_KEY']}:#{ENV["SHOPIFY_API_SECRET"]}@#{ENV["SHOPIFY_HOST"]}.myshopify.com/admin"
 ShopifyAPI::Base.site = shop_url
 
 
 puts "---Checking Shopify Authentication---"
 shop = ShopifyAPI::Shop.current
+puts shop
 puts "----"
 
 set :protection, :except => :frame_options
+enable :sessions
 
 #####        #####
 ###   Routes   ###
 #####        #####
 
+before do 
+  pass if request.path_info == '/'
+  
+  if session['auth'] then pass else halt 401 end
+end
+
 get '/' do
-  haml :general_error, :layout => :shopify
+  halt 401
 end
 
 post '/' do 
   result = verify_and_return_context(request)
-
+  session['auth'] = result[0]
   shopify_customer = search_shopify_customer(result[1])
 
-  if result[0]
-    if shopify_customer
-      haml :index, :layout => :shopify, :locals => {:customer => shopify_customer, :orders => shopify_customer.orders}
-    else
-      haml :order, :layout => :shopify
-    end
+  halt 401 unless session['auth']
+
+  if shopify_customer 
+    haml :index, :layout => :shopify, :locals => {:customer => shopify_customer, :orders => shopify_customer.orders}
   else
-    haml :general_error, :layout => :shopify
+    haml :order, :layout => :shopify
   end
 end
 
@@ -49,6 +51,7 @@ post '/order_search' do
   order_search_result = search_shopify_order(order_search)
   haml :order_search_results, :layout => :shopify, :locals => {:order=> order_search_result}
 end
+
 #####         #####
 ###   HELPERS   ###
 #####         #####
@@ -58,31 +61,29 @@ end
 
 def verify_and_return_context(request)
   request.body.rewind  # in case someone already read it
-  canvas_key = ENV['CANVAS_KEY']
 
   data = URI.unescape(request.body.read.to_s.split('signed_request=')[1])
   hashed_context = data.split('.')[0]
   context        = data.split('.')[1]
 
-  signed_context = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), canvas_key, context)
+  signed_context = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), ENV['CANVAS_KEY'], context)
   hash = Base64.strict_encode64(signed_context).strip()
-  puts "---Request Verified?---"
-  puts hashed_context == hash
-  puts "-----"
   return [hashed_context == hash, Base64.decode64(context)]
 end
 
 #Searches Shopify for a customer based on the Desk context
+#returns a shopify customer object
 def search_shopify_customer(context)
   context = JSON.parse(context)
   customer_emails = context['context']['environment']['customer']['emailAddresses']
   customer_email = customer_emails.length > 0 ? customer_emails.first['value'] : ""
 
   @shopify_customer = ShopifyAPI::Customer.search(query: customer_email).first
-  puts @shopify_customer
   return @shopify_customer
 end
 
+#Searches Shopify for a specific order by name or order_id
+#Returns a single order object
 def search_shopify_order(search)
   count = ShopifyAPI::Order.count
   pages = count / 250
@@ -101,5 +102,6 @@ def search_shopify_order(search)
       pages -= 1
       break if order
   end while pages > 0
+
   return order
 end
