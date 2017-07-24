c757b19e70b8d3304dac562d669be986b28b68c4
kpsf <kprodehl@salesforce.com>
2017-07-24 10:51:39 -0700

    code cleanup

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require 'json'
 shop_url = "https://#{ENV['SHOPIFY_API_KEY']}:#{ENV["SHOPIFY_API_SECRET"]}@#{ENV["SHOPIFY_HOST"]}.myshopify.com/admin"
 ShopifyAPI::Base.site = shop_url
 
+puts shop_url
 
 puts "---Checking Shopify Authentication---"
 shop = ShopifyAPI::Shop.current
@@ -32,11 +33,11 @@ get '/' do
 end
 
 post '/' do 
-  result = verify_and_return_context(request)
-  session['auth'] = result[0]
-  shopify_customer = search_shopify_customer(result[1])
+  session['auth'] = verify_context unless session['auth']
 
   halt 401 unless session['auth']
+  
+  shopify_customer = search_shopify_customer(decode_context)
 
   if shopify_customer 
     haml :index, :layout => :shopify, :locals => {:customer => shopify_customer, :orders => shopify_customer.orders}
@@ -56,38 +57,69 @@ end
 ###   HELPERS   ###
 #####         #####
 
+##
 #Parses the canvas request body and a verifies the CanvasObject based on the shared key.
-#Returns: [verified true/false, decoded context]
+#
+def verify_context
+  data = parsed_request.split('.')
+  hashed_context = data[0]
+  @context       = data[1]
 
-def verify_and_return_context(request)
-  request.body.rewind  # in case someone already read it
+  hash = Base64.strict_encode64(signed_context).strip()
+  return hashed_context == hash
+end
+
+##
+#Sinatra request body needs to be uri unescaped for some reason
+#
+def parsed_request
+  request.body.rewind
 
   data = URI.unescape(request.body.read.to_s.split('signed_request=')[1])
-  hashed_context = data.split('.')[0]
-  context        = data.split('.')[1]
+end
 
-  signed_context = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), ENV['CANVAS_KEY'], context)
-  hash = Base64.strict_encode64(signed_context).strip()
-  return [hashed_context == hash, Base64.decode64(context)]
+##
+#Sign the context with the canvas shared key
+#
+def signed_context
+  signed_context = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), ENV['CANVAS_KEY'], @context)
+end
+
+##
+#Parses context and base64 decodes
+#
+def decode_context
+  data = parsed_request
+  
+  context = Base64.decode64(data.split('.')[1])
 end
 
 #Searches Shopify for a customer based on the Desk context
 #returns a shopify customer object
 def search_shopify_customer(context)
+  customer_emails = context_customer_emails(context)
+  shopify_customer = nil
+
+  customer_emails.each do |customer_email|
+    shopify_customer = ShopifyAPI::Customer.search(query: customer_email).first
+    break if shopify_customer
+  end
+
+  return shopify_customer
+end
+
+#returns an array of customer emails from the raw context
+def context_customer_emails(context)
   context = JSON.parse(context)
   customer_emails = context['context']['environment']['customer']['emailAddresses']
-  customer_email = customer_emails.length > 0 ? customer_emails.first['value'] : ""
-
-  @shopify_customer = ShopifyAPI::Customer.search(query: customer_email).first
-  return @shopify_customer
+  customer_emails = customer_emails.map {|x| x["value"]}
 end
 
 #Searches Shopify for a specific order by name or order_id
 #Returns a single order object
 def search_shopify_order(search)
   count = ShopifyAPI::Order.count
-  pages = count / 250
-  pages = pages.to_i + 1
+  pages = (count / 250) + 1
   order = nil
 
   begin
